4bab36309f69f995457696769efc5134b3a922a1
FerralG <mferralg@hotmail.com>
2026-05-25 10:56:08 -0600

    arreglo de backend

--- src/models/propiedadesModel.js
+++ src/models/propiedadesModel.js
@@ -3,8 +3,24 @@ const pool = require('../db');
 const obtenerPropiedades = async () => {
 
     const result = await pool.query(`
-        SELECT *
+        SELECT
+            id,
+            title,
+            type,
+            bedrooms,
+            bathrooms,
+            parking,
+            imageurl AS "imageUrl",
+            locationname AS "locationName",
+            operation,
+
+            CASE
+                WHEN operation = 'renta' THEN renta_precio
+                ELSE venta_precio
+            END AS price
+
         FROM propiedades
+
         ORDER BY id DESC
     `);
 
