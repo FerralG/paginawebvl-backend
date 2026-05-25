2bf5d808290f69f734bea381687d1bd94cc1bb08
FerralG <mferralg@hotmail.com>
2026-05-25 02:01:52 -0600

    ahora si la buena

--- src/app.js
+++ src/app.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-const pool = require('./db');
+const propiedadesRoutes = require('./routes/propiedades.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-
-  try {
-
-    const result = await pool.query('SELECT NOW()');
-
-    res.json({
-      message: 'API funcionando 🚀',
-      database_time: result.rows[0]
-    });
-
-  } catch (error) {
-
-    console.error(error);
-
-    res.status(500).json({
-      error: 'Error conectando a PostgreSQL'
-    });
-
-  }
-
-});
+app.use('/propiedades', propiedadesRoutes);
 
 module.exports = app;
--- src/routes/propiedadesRoutes.js
+++ src/routes/propiedadesRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const propiedadesController = require('../controllers/propiedades.controller');
+
 router.get('/', propiedadesController.getPropiedades);
+
+module.exports = router;
